aba532ce568ee1fc9a0b36603feb7ccb15945a92
Simon <simsn0401@gmail.com>
2024-02-24 12:04:33 +0100

    Initial Commit

---
+++ BartenderBot/BartenderBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  BartenderBotApp.swift
+//  BartenderBot
+//
+//  Created by Simon Raass on 24.02.24.
+//
+
+import SwiftUI
+
+@main
+struct BartenderBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
